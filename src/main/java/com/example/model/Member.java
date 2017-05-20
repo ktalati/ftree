d149997c0f60a79537e3596d35dccf853ad690d9
Savan Ladani <savan.ladani@gmail.com>
2017-05-20 18:02:32 +0530

    user avatar completed

--- src/main/java/com/example/model/Member.java
+++ src/main/java/com/example/model/Member.java
@@ -1,14 +1,30 @@
 package com.example.model;
 
-import org.hibernate.validator.constraints.Email;
-import org.hibernate.validator.constraints.NotEmpty;
-import org.springframework.format.annotation.DateTimeFormat;
-
-import javax.persistence.*;
-import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+import javax.validation.Valid;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Table(name = "member")
@@ -76,6 +92,10 @@ public class Member {
 
     @Column(name = "mother_name")
     private String motherValue;
+
+    @Column(name = "avatar")
+    @Lob
+    private byte[] avatar;
 
     @Transient
     private String fatherMemberValue;
@@ -246,4 +266,12 @@ public class Member {
     public void setMotherValue(String motherValue) {
         this.motherValue = motherValue;
     }
+
+	public byte[] getAvatar() {
+		return avatar;
+	}
+
+	public void setAvatar(byte[] avatar) {
+		this.avatar = avatar;
+	}
 }
